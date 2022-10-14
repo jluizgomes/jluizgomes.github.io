0cae60fe6bd0a40471ebb86d2b7b197b7cad2c4b
Jorge Luiz Gomes <jluizgomes@gmail.com>
2022-10-14 15:25:16 -0300

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -15,7 +15,7 @@ const Menu: React.FC<Props> = () => {
     <div className="menu">
         {/* <Link href="/"><a className={router.pathname === '/' ? 'active' : ''}>Home</a></Link> */}
         <Link href="/"><a className={router.pathname === '/' ? 'active' : ''}>About Me</a></Link>
-        <Link href="/projects"><a className={router.pathname === '/projects' ? 'active' : ''}>Projects</a></Link>
+        {/* <Link href="/projects"><a className={router.pathname === '/projects' ? 'active' : ''}>Projects</a></Link> */}
         <Link href="/blog"><a className={router.pathname === '/blog' ? 'active' : ''}>Blog</a></Link>
         <Link href="/contact"><a className={router.pathname === '/contact' ? 'active' : ''}>Contact</a></Link>
       </div>
