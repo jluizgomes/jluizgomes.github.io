11104778d6e53ef7a2fe592015a4504329fa00c8
Jorge Luiz Gomes <jluizgomes@gmail.com>
2022-10-14 14:24:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: isProd ? '/jluizgomes.github.io/' : '',
   assetPrefix: isProd ? '/jluizgomes.github.io/' : '',
   images: {
     unoptimized: true,
